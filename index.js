6f433f582af5fca22073dca10f1b3149dc9dce72
Mitzi17 <m_estrada17@live.com>
2023-05-06 19:51:44 -0700

    added comments

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Defined the three classes: Task, List, and Menu.
+// In this application a list can have many tasks and a task belongs to a list.
 class Task {
 
     constructor(name) {
@@ -11,7 +13,7 @@ class Task {
 }
 
 class List {
-
+// Since a list can have many tasks, the property tasks is set to an empty array.
     constructor(title) {
         this.title = title;
         this.tasks = [];
@@ -27,12 +29,14 @@ class List {
 
 }
 
+// The class Menu determines how we navigate through the application.
 class Menu {
     constructor() {
         this.lists = [];
         this.selectedList = null;
     }
 
+// This start() method is called to start the application.
     start() {
         let selection = this.showMainMenuOptions();
 
@@ -78,6 +82,7 @@ class Menu {
         `);
     }
 
+    // Methods to create, view, and delete a list.
     displayAllLists() {
         let listString = '';
         for (let i = 0; i < this.lists.length; i++) {
@@ -122,6 +127,7 @@ class Menu {
         }
     }
 
+// Methods to create and delete a task.
     createTask() {
         let name = prompt('Enter name or description of new task:');
         this.selectedList.addTask(new Task(name));
@@ -136,5 +142,7 @@ class Menu {
 
 }
 
+// This creates a new instance of Menu and called the start() method on it to start the application.
+// The start() method is defined inside the Menu class.
 let menu = new Menu()
 menu.start()
